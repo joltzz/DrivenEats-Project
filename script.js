374e29230fd12417fc909c8276866b71f7a40881
joltzz <guijsilva02@gmail.com>
2022-01-26 14:53:49 -0300

    primeiro envio driveneats

---
+++ script.js
@@ -0,0 +1,40 @@
+
+//Função da borda verde dos pratos
+
+function escolherFrango(){
+    document.getElementById("frango").style.borderColor = "#32B72F";
+}
+function escolherFrango2(){
+    document.getElementById("frango2").style.borderColor = "#32B72F";
+}
+function escolherFrango3(){
+    document.getElementById("frango3").style.borderColor = "#32B72F";
+}
+function escolherFrango4(){
+    document.getElementById("frango4").style.borderColor = "#32B72F";
+}
+
+
+
+//Função da borda verde das bebidas
+
+function escolherBebida(){
+    document.getElementById("coca").style.borderColor = "#32B72F";
+}
+function escolherBebida2(){
+    document.getElementById("coca2").style.borderColor = "#32B72F";
+}
+function escolherBebida3(){
+    document.getElementById("coca3").style.borderColor = "#32B72F";
+}
+function escolherBebida4(){
+    document.getElementById("coca4").style.borderColor = "#32B72F";
+}
+
+
+
+//Função da borda verde nas sobremesas
+
+function escolherSobremesa(){
+    
+}
